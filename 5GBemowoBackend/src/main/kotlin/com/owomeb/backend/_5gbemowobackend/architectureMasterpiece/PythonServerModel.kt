b98d982c12ebf1d16aef83d17ef8b39e5ead3603
bsawick1 <01187375@pw.edu.pl>
2025-06-30 13:10:59 +0200

    zmiany w PythonServerModel.kt

--- 5GBemowoBackend/src/main/kotlin/com/owomeb/backend/_5gbemowobackend/architectureMasterpiece/PythonServerModel.kt
+++ 5GBemowoBackend/src/main/kotlin/com/owomeb/backend/_5gbemowobackend/architectureMasterpiece/PythonServerModel.kt
@@ -35,7 +35,7 @@ abstract class PythonServerModel<T>(
     private fun startServer() {
         thread {
             try {
-                val processBuilder = ProcessBuilder("python",
+                val processBuilder = ProcessBuilder("python3",
                     scriptPath,
                     serverName,
                     actualPort.toString()
